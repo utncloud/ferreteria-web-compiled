64cf3c13f037eb9494920b9b34571f7950155c18
Ever Barahona <ebarahona@utn.ac.cr>
2022-08-27 10:49:22 -0600

    adding dockerfile

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my-angular-app'),
+      dir: require('path').join(__dirname, './coverage/tools-store-web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-angular-app'`, () => {
+  it(`should have as title 'tools-store-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-angular-app');
+    expect(app.title).toEqual('tools-store-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-angular-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('tools-store-web app is running!');
   });
 });
